f093bafb8a13dc3a176eac6a8ab29a995e8a8022
gals1707 <shechter.gal@gmail.com>
2018-05-10 23:49:44 +0300

    changes to 20

--- shareCal/quickstart.js
+++ shareCal/quickstart.js
@@ -77,14 +77,14 @@ function listEvents(auth) {
   calendar.events.list({
     calendarId: 'primary',
     timeMin: (new Date()).toISOString(),
-    maxResults: 10,
+    maxResults: 20,
     singleEvents: true,
     orderBy: 'startTime',
   }, (err, {data}) => {
     if (err) return console.log('The API returned an error: ' + err);
     const events = data.items;
     if (events.length) {
-      console.log('Upcoming 10 events:');
+      console.log('Upcoming 20 events:');
       events.map((event, i) => {
         const start = event.start.dateTime || event.start.date;
         console.log(`${start} - ${event.summary}`);
