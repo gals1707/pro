dd0802fe7165a37f487d11a5e8866c1910a1f239
omer2500 <omer2500@gmail.com>
2018-05-13 23:09:51 +0300

    ated add event method

--- shareCal/quickstart.js
+++ shareCal/quickstart.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
 const OAuth2Client = google.auth.OAuth2;
-const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 const TOKEN_PATH = 'credentials.json';
 
 // Load client secrets from a local file.
 try {
   const content = fs.readFileSync('client_secret.json');
-  authorize(JSON.parse(content), listEvents);
+  authorize(JSON.parse(content), addEvent);
 } catch (err) {
   return console.log('Error loading client secret file:', err);
 }
@@ -93,4 +93,52 @@ function listEvents(auth) {
       console.log('No upcoming events found.');
     }
   });
+}
+
+// Refer to the Node.js quickstart on how to setup the environment:
+// https://developers.google.com/calendar/quickstart/node
+// Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
+// stored credentials.
+function addEvent(auth){
+var event = {
+  'summary': 'Google I/O 2015',
+  'location': '800 Howard St., San Francisco, CA 94103',
+  'description': 'A chance to hear more about Google\'s developer products.',
+  'start': {
+    'dateTime': '2018-05-28T09:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+  },
+  'end': {
+    'dateTime': '2018-05-28T17:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+  },
+  'recurrence': [
+    'RRULE:FREQ=DAILY;COUNT=2'
+  ],
+  'attendees': [
+    {'email': 'lpage@example.com'},
+    {'email': 'sbrin@example.com'},
+  ],
+  'reminders': {
+    'useDefault': false,
+    'overrides': [
+      {'method': 'email', 'minutes': 24 * 60},
+      {'method': 'popup', 'minutes': 10},
+    ],
+  },
+};
+const calendar = google.calendar({version: 'v3', auth});
+
+calendar.events.insert({
+  auth: auth,
+  calendarId: 'primary',
+  resource: event,
+}, function(err, event) {
+  if (err) {
+    console.log('There was an error contacting the Calendar service: ' + err);
+    return;
+  }
+  console.log('Event created: %s', event.htmlLink);
+});
+
 }
